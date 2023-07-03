b7109c580ac3c86f99b64dfab3c0d74cf903e662
Dipshi Verma <vermadipshi70@gmail.com>
2023-07-03 18:00:01 +0530

    add link

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -60,7 +60,7 @@ const Projects = () => {
         title="GOOGLE DOCS"
         des=" The Google Docs is a fully functional web application that replicates the features of Google Docs, including editing the title and the document, and the ability to download the document as well as all the formatting options are functional. "
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsKTed-jxS2hnsRF_HDKj2OfKyTYGss24EIWx-aJS60A&s"
-        githublink="https://github.com/DipshiV/Google-Docs"
+        githublink="https://github.com/DipshiV/Google-Docs/tree/master"
         deloymentlink= "https://google-docs-dusky.vercel.app/"
 
       />
